79c008396c73eb6784e0178097b832a071a016aa
haosui <pvdung0310@gmail.com>
2023-12-11 01:15:52 +0700

    3

--- src/App.js
+++ src/App.js
@@ -4,30 +4,41 @@ function App() {
   const [userInfo, setUserInfo] = useState(null);
 
   useEffect(() => {
-    const fetchUserInfo = async () => {
-      try {
-        const response = await fetch('/.auth/me');
-        const user = await response.json();
-        setUserInfo(user);
-      } catch (error) {
-        console.error('Error fetching user info:', error);
-      }
-    };
-
-    fetchUserInfo();
+    (async () => {
+      setUserInfo(await getUserInfo());
+    })();
   }, []);
+
+  async function getUserInfo() {
+    try {
+      const response = await fetch('/.auth/me');
+      const payload = await response.json();
+      const { clientPrincipal } = payload;
+      return clientPrincipal;
+    } catch (error) {
+      console.error('No profile could be found');
+      return undefined;
+    }
+  }
+  const redirect = window.location.pathname;
 
   return (
     <div>
       <h1>Azure Static Web Apps custom roles demo</h1>
       <div>
-        <a href="/.auth/login/aad">Login</a> | <a href="/.auth/logout">Logout</a>
+      {!userInfo &&
+              <a  href={`/.auth/login/aad?post_login_redirect_uri=${redirect}`}>
+                login
+              </a>
+            } 
+            {userInfo && <a href={`/.auth/logout?post_logout_redirect_uri=${redirect}`}>Logout</a>}
       </div>
       {userInfo && (
         <div>
           Hello {userInfo.clientPrincipal && userInfo.clientPrincipal.userDetails}
         </div>
       )}
+      <a>saa</a>
     </div>
   );
 }
