b59a05fddb60c1251e4f7ca2e25f807499cc088a
haosui <pvdung0310@gmail.com>
2023-12-11 00:30:09 +0700

    1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const response = await fetch('/.auth/me');
+        const user = await response.json();
+        setUserInfo(user);
+      } catch (error) {
+        console.error('Error fetching user info:', error);
+      }
+    };
+
+    fetchUserInfo();
+  }, []);
+
+  return (
+    <div>
+      <h1>Azure Static Web Apps custom roles demo</h1>
+      <div>
+        <a href="/.auth/login/aad">Login</a> | <a href="/.auth/logout">Logout</a>
+      </div>
+      {userInfo && (
+        <div>
+          Hello {userInfo.clientPrincipal && userInfo.clientPrincipal.userDetails}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
